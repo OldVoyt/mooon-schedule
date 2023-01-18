c22c39fc61bd686254504c3203c133b2148e7bcf
multistime <multistime@gmail.com>
2023-01-18 11:06:24 +0300

    cinema names corrected

--- src/types/Settings.ts
+++ src/types/Settings.ts
@@ -1,9 +1,9 @@
 import { Theatre } from './ScheduleTypes'
 
 export const TheatresAvailable: Theatre[] = [
-  { Id: '3', Name: 'VOKA CINEMA by Silver Screen в ТРЦ "Dana Mall"' },
-  { Id: '19', Name: 'mooon в ТРЦ "Palazzo"' },
-  { Id: '11', Name: 'mooon в ТРК "Triniti"' },
-  { Id: '2', Name: 'Silver Screen cinemas в ТРЦ "ArenaCity"' }
+  { Id: '3', Name: 'mooon в ТРЦ Dana Mall' },
+  { Id: '19', Name: 'mooon в ТРЦ Palazzo' },
+  { Id: '11', Name: 'mooon в ТРК Triniti' },
+  { Id: '2', Name: 'Silver Screen в ТРЦ ArenaCity' }
 ]
 export const DaysOffsetAvailable: number[] = [0, 1]
